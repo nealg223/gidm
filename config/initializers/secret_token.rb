0661e39b47a2d887aa7d407122d684412d1c66a7
Neal Sales-Griffin <nealsalesgriffin@gmail.com>
2011-01-24 12:14:13 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gidm::Application.config.secret_token = 'a8232ad74c46d45715e0304295dec49bf01a824c80ad67e366617cc0076a705005779fe6e2ea502835b5ebed29ca30d3e3177de755247751837d2ea53d3abc73'
